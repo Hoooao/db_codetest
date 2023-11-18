8eeeb712358877c6d206d6a9284afa9eacd84e9d
Hoooao <hao021014@163.com>
2023-11-17 22:27:14 -0500

    deadlock resolved

--- src/data.hpp
+++ src/data.hpp
@@ -3,55 +3,64 @@
 #include <mutex>
 #include <shared_mutex>
 
+// LOCK_SEG_SIZE has to be greater than 1
+constexpr int LOCK_SEG_SIZE = 2;
 class DataList{
 public:
     DataList(int size): dl_size(size){
-        assert(size >= 100);
+        assert(LOCK_SEG_SIZE > 1);
+        assert(size >= LOCK_SEG_SIZE);
         // Initialize to all 1s
         data_list = std::vector<u_int>(size,1);
         // 100 consecutive elements share a lock
-        locks = std::vector<std::shared_mutex>(size/100+1);
-        std::clog<<"DataList initialized"<<std::endl;
+        locks = std::vector<std::shared_timed_mutex>(size/LOCK_SEG_SIZE+1);
+        // std::clog<<"DataList initialized"<<std::endl;
     }
     void set_j_with_i(int i, int j){
+
         int i_1 = (i+1)%dl_size;
         int i_2 = (i+2)%dl_size;
-        int lock_i = i/100;
-        int lock_i_2 = i_2/100;
-        int lock_j = j/100;
-        std::unique_lock<std::shared_mutex> w_lock(locks[lock_j],std::defer_lock);
+        int lock_i = i/LOCK_SEG_SIZE;
+        int lock_i_2 = i_2/LOCK_SEG_SIZE;
+        int lock_j = j/LOCK_SEG_SIZE;
 
-        if(lock_j != lock_i && lock_j != lock_i_2){
-            // lock of j is not that of i~i+2
-            if (lock_i!=lock_i_2){
-                std::shared_lock<std::shared_mutex> r_lock_0(locks[lock_i]);
-                std::shared_lock<std::shared_mutex> r_lock_2(locks[lock_i_2]);
-                w_lock.lock();
-                data_list[j] = data_list[i_1]+data_list[i_2]+data_list[i];
-            }else{
-                std::shared_lock<std::shared_mutex> r_lock_0(locks[lock_i]);
-                w_lock.lock();
-                data_list[j] = data_list[i_1]+data_list[i_2]+data_list[i];
+        while (true){
+
+            // Process j overlaps with i~i+2
+            std::shared_lock<std::shared_timed_mutex> r_lock_0(locks[lock_i], std::defer_lock);
+            std::shared_lock<std::shared_timed_mutex> r_lock_2(locks[lock_i_2], std::defer_lock);
+            if(lock_j != lock_i && lock_j != lock_i_2){
+                // lock of j is not those of i~i+2
+                if (lock_i!=lock_i_2){
+                    if (!r_lock_2.try_lock_for(std::chrono::milliseconds(2))) {
+                        continue;
+                    }
+                }
+                if (!r_lock_0.try_lock_for(std::chrono::milliseconds(2))) {
+                    continue;
+                }
+            }else if (lock_j != lock_i_2){
+                // l_j == l_i
+                if (!r_lock_2.try_lock_for(std::chrono::milliseconds(2))) {
+                    continue;
+                }
+            }else if (lock_j != lock_i) {
+                // l_j == l_i_2
+                if (!r_lock_0.try_lock_for(std::chrono::milliseconds(2))) {
+                    continue;
+                }
             }
-        }else if (lock_j != lock_i_2){
-            // l_j == l_i
-            std::shared_lock<std::shared_mutex> r_lock_2(locks[lock_i_2]);
-            w_lock.lock();
+            std::unique_lock<std::shared_timed_mutex> w_lock(locks[lock_j], std::defer_lock);
+            if (!w_lock.try_lock_for(std::chrono::milliseconds(2))) {
+                continue;
+            }
             data_list[j] = data_list[i_1]+data_list[i_2]+data_list[i];
-        }else if (lock_j != lock_i) {
-            // l_j == l_i_2
-            std::shared_lock <std::shared_mutex> r_lock_0(locks[lock_i]);
-            w_lock.lock();
-            data_list[j] = data_list[i_1] + data_list[i_2] + data_list[i];
-        }else{
-            // l_j == l_i == l_i_2
-            w_lock.lock();
-            data_list[j] = data_list[i_1]+data_list[i_2]+data_list[i];
+            break;
         }
     }
 
 private:
     u_int dl_size;
     std::vector<u_int> data_list;
-    std::vector<std::shared_mutex> locks;
+    std::vector<std::shared_timed_mutex> locks;
 };
--- src/db.cpp
+++ src/db.cpp
@@ -8,7 +8,7 @@ DataList dl(DATA_SIZE);
 void worker(){
     for (int k = 0; k < REPETITION; k++) {
         if (k%500 == 0){
-            std::clog<<"Worker "<<std::this_thread::get_id()<<" finished "<<k<<" iterations"<<std::endl;
+           // std::clog<<"Worker "<<std::this_thread::get_id()<<" finished "<<k<<" iterations"<<std::endl;
         }
         int j = rand() % (DATA_SIZE+1);
         int i = rand() % (DATA_SIZE+1);
@@ -19,8 +19,9 @@ void worker(){
 int main(int argc, char* argv[]){
     if (argc < 2) {
         std::cerr << "Arguments needed: num_workers" << std::endl;
-        exit(1);
+        exit(1);;
     }
+    std::chrono::time_point start = std::chrono::high_resolution_clock::now();
     int num_workers = atoi(argv[1]);
     std::thread workers[num_workers];
     for (int i = 0; i < num_workers; i++) {
@@ -29,4 +30,7 @@ int main(int argc, char* argv[]){
     for(auto& t : workers){
         t.join();
     }
+    std::chrono::time_point end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> diff = end-start;
+    std::clog<<"Time elapsed: "<<diff.count()<<" ms"<<std::endl;
 }
